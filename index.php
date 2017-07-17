1df8bcdaa3ab373f60c47f51eccf87984bdcb771
Ibrahim Oluwadamilare <Oluwadamilare@Oluwadamilare.local>
2017-07-17 16:25:48 +0100

    updated the content and some images

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta name="keywords" content="Mango Social Inc. " />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <!-- SITE TITLE -->
-    <title>Mango Social Inc</title>
+    <title>Mango Social</title>
 
     <!-- FAVICON -->
     <link rel="icon" href="images/favicon.ico">
@@ -76,14 +76,14 @@
         <div class="wrap">
             <div class="section-image"> 
             <!-- Home IMAGE --><img src="images/appscreen1.png" alt="Home">
-            <!-- About IMAGE --><img src="images/appscreen1.png" alt="About">
-            <!-- Features IMAGE --><img src="images/appscreen1.png" alt="Features">
+            <!-- About IMAGE --><img src="images/appscreen2.jpg" alt="About">
+            <!-- Features IMAGE --><img src="images/appscreen3.jpg" alt="Features">
              <!--Video IMAGE <img src="http://placehold.it/232x407" alt="Video">
              Clients IMAGE <img src="http://placehold.it/232x407" alt="Clients">-->
             <!-- Screenshots IMAGE --><img src="images/appscreen1.png" alt="Screenshots">
              <!--Pricing IMAGE <img src="http://placehold.it/232x407" alt="Pricing">-->
             <!-- Download IMAGE --><img src="images/appscreen1.png" alt="Download">
-            <!-- Contact IMAGE --><img src="images/appscreen1.png" alt="Contact">
+            <!-- Contact IMAGE --><img src="images/appscreen3.jpg" alt="Contact">
             </div>
             <div id="hand"></div>
         </div>
@@ -95,86 +95,13 @@
             <div class="wrap">
                 <div class="box">
                     <!-- SECTION HOME CONTENT -->
-                    <h1><strong>Mongo</strong> Social Inc.</h1>
-                    <p>A better way to present your app using fully featured appsperia template.
+                    <h1><strong>Mongo</strong> Social.</h1>
+                    <p style="text-align:justify">Mongo Social is an all around social networking experience for iOS user's specializing in live video chat and feed complimented by both traditional and innovative features! </p>
                     <br> Now available on the App Store and Play Store!</p> <a href="#Download" class="simple-button"><span class="icon flaticon-download7"></span>Download App</a> </div>
                     <!-- END SECTION HOME CONTENT -->
             </div>
         </div>
         <!-- END SECTION HOME -->
-
-            <!-- SECTION ABOUT -->
-        <div class="section" id="section1">
-            <div class="wrap">
-                <div class="box">
-                    <!-- SECTION ABOUT CONTENT -->
-                    <h2>About <strong>Mongo </strong>Social App</h2>
-                    <div class="tabs tabs-style-linemove">
-                        <!-- TABS LINKS -->
-                        <nav>
-                            <ul>
-                                <li><a href="#section-linemove-1"><span class="icon flaticon-lightbulb"></span><span> Design</span></a>
-                                </li>
-                                <li><a href="#section-linemove-2"><span class="icon flaticon-adjust3"></span><span> Features</span></a>
-                                </li>
-                                <li><a href="#section-linemove-3"><span class="icon flaticon-drawer1"></span><span> Demos</span></a>
-                                </li>
-                                <li><a href="#section-linemove-4"><span class="icon flaticon-laptop3"></span><span> Elements</span></a>
-                                </li>
-                            </ul>
-                        </nav>
-                        <!-- END TABS LINKS -->
-
-                        <!-- TABS CONTENT -->
-                        <div class="content-wrap">
-                           
-                            <!-- TAB 1 -->
-                            <section id="section-linemove-1">
-                                <h4><span class="icon flaticon-lightbulb"></span> Good design doesn't date!</h4>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora similique excepturi obcaecati, maiores nostrum esse illo in soluta at saepe perspiciatis eos quasi laudantium sunt ad quaerat ipsum dolor sit amet, consectetur adipisicing elit?</p>
-                            </section>
-
-                             <!-- TAB 2 -->
-                            <section id="section-linemove-2">
-                                <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora similique excepturi obcaecati, aiores nostrum esse illo in soluta.</h4>
-                                <ul class="features">
-                                    <li><a class="tooltip" href="#"><span class="icon flaticon-desktop1"></span> Unique Design</a> </li>
-                                    <li><a class="tooltip" href="#"><span class="icon flaticon-small62"></span> Well Documented</a> </li>
-                                </ul>
-                                <ul class="features">
-                                    <li><a class="tooltip" href="#"><span class="icon flaticon-outlined3"></span> Multiple Demos</a> </li>
-                                    <li><a class="tooltip" href="#"><span class="icon flaticon-lightbulb"></span> Amazing Features</a> </li>
-                                </ul>
-                            </section>
-
-                            <!-- TAB 3 -->
-                            <section id="section-linemove-3">
-                                <h4><span class="icon flaticon-drawer1"></span> Check out Appsperia's multiple demos!</h4>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora similique excepturi obcaecati, maiores nostrum esse illo in soluta at saepe perspiciatis eos quasi laudantium sunt ad quaerat ipsum dolor sit amet, consectetur adipisicing elit?</p>
-                            </section>
-                            
-                            <!-- TAB 4 -->
-                            <section id="section-linemove-4">
-                                <p>
-                                    <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora similique excepturi obcaecati, aiores nostrum esse illo in soluta.</h4>
-                                    <ul class="features">
-                                        <li><a class="tooltip" href="#"><span class="icon flaticon-desktop1"></span> Content Boxes</a> </li>
-                                        <li><a class="tooltip" href="#"><span class="icon flaticon-small62"></span> Extended Tabs</a> </li>
-                                    </ul>
-                                    <ul class="features">
-                                        <li><a class="tooltip" href="#"><span class="icon flaticon-outlined3"></span> Carousels & Sliders</a> </li>
-                                        <li><a class="tooltip" href="#"><span class="icon flaticon-lightbulb"></span> Contact Form</a> </li>
-                                    </ul>
-                            </section>
-                        </div>
-                         <!-- END TABS CONTENT -->                            
-
-                    <!-- END SECTION ABOUT -->
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- END SECTION ABOUT -->    
 
         <!-- SECTION FEATURES -->
         <div class="section " id="section2">
@@ -182,16 +109,16 @@
                 <div class="box">
                     <!-- SECTION FEATURES CONTENT -->
                     <h2><strong>Amazing</strong> Features</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora similique excepturi obcaecati, maiores nostrum esse illo in soluta at saepe perspiciatis eos quasi laudantium sunt ad quaerat?</p>
+
                     <ul class="features">
-                        <li><a class="tooltip" href="#"><span class="icon flaticon-desktop1"></span> Clean Design<span class="tooltip-content"><span class="tooltip-text"><span class="tooltip-inner"><span class="icon flaticon-desktop1"></span>Lorem ipsum dolor sit amet, consectetur adipisicing elitr eiciendis autem aperiam.</span></span></span></a> </li>
-                        <li><a class="tooltip" href="#"><span class="icon flaticon-small62"></span> Dedicated Support<span class="tooltip-content"><span class="tooltip-text"><span class="tooltip-inner"><span class="icon flaticon-small62"></span>Lorem ipsum dolor sit amet, consectetur adipisicing elitr eiciendis autem aperiam.</span></span></span></a> </li>
-                        <li><a class="tooltip" href="#"><span class="icon flaticon-adjust3"></span> Very customizable<span class="tooltip-content"><span class="tooltip-text"><span class="tooltip-inner"><span class="icon flaticon-adjust3"></span>Lorem ipsum dolor sit amet, consectetur adipisicing elitr eiciendis autem aperiam.</span></span></span></a> </li>
+                        <li><a class="tooltip" href="#"><span class="icon flaticon-desktop1"></span> 1:1,1:2,2:1,2:2 live video chat<span class="tooltip-content"><span class="tooltip-text"><span class="tooltip-inner"><span class="icon flaticon-desktop1"></span></span></span></span></a> </li>
+                        <li><a class="tooltip" href="#"><span class="icon flaticon-small62"></span>Fully video operated <span class="tooltip-content"><span class="tooltip-text"><span class="tooltip-inner"><span class="icon flaticon-small62"></span></span></span></span></a> </li>
+                        <li><a class="tooltip" href="#"><span class="icon flaticon-adjust3"></span>Emo-gifs and emo-gifs keyboard<span class="tooltip-content"><span class="tooltip-text"><span class="tooltip-inner"><span class="icon flaticon-adjust3"></span></span></span></span></a> </li>
                     </ul>
                     <ul class="features">
-                        <li><a class="tooltip" href="#"><span class="icon flaticon-interface19"></span> Well Documented<span class="tooltip-content"><span class="tooltip-text"><span class="tooltip-inner"><span class="icon flaticon-interface19"></span>Lorem ipsum dolor sit amet, consectetur adipisicing elitr eiciendis autem aperiam.</span></span></span></a> </li>
-                        <li><a class="tooltip" href="#"><span class="icon flaticon-outlined3"></span> Multiple Demos<span class="tooltip-content"><span class="tooltip-text"><span class="tooltip-inner"><span class="icon flaticon-outlined3"></span>Lorem ipsum dolor sit amet, consectetur adipisicing elitr eiciendis autem aperiam.</span></span></span></a> </li>
-                        <li><a class="tooltip" href="#"><span class="icon flaticon-lightbulb"></span> Amazing Features<span class="tooltip-content"><span class="tooltip-text"><span class="tooltip-inner"><span class="icon flaticon-lightbulb"></span>Lorem ipsum dolor sit amet, consectetur adipisicing elitr eiciendis autem aperiam.</span></span></span></a> </li>
+                        <li><a class="tooltip" href="#"><span class="icon flaticon-interface19"></span>Interactive multi user filter(s) <span class="tooltip-content"><span class="tooltip-text"><span class="tooltip-inner"><span class="icon flaticon-interface19"></span></span></span></span></a> </li>
+                        <li><a class="tooltip" href="#"><span class="icon flaticon-outlined3"></span>360° Photos and Video <span class="tooltip-content"><span class="tooltip-text"><span class="tooltip-inner"><span class="icon flaticon-outlined3"></span></span></span></span></a> </li>
+                        <li><a class="tooltip" href="#"><span class="icon flaticon-lightbulb"></span> User's can host timed video chat parties <span class="tooltip-content"><span class="tooltip-text"><span class="tooltip-inner"><span class="icon flaticon-lightbulb"></span></span></span></span></a> </li>
                     </ul>
                     <!-- END SECTION FEATURES CONTENT -->
                 </div>
@@ -261,12 +188,8 @@
                 <div class="box">
                     <div class="fp-tableCell screenshots-content">
                         <!-- SECTION SCREENSHOTS CONTENT -->
-                        <h2>Screenshots</h2>
-                        <ul class="features">
-                            <li><a class="tooltip"><span class="icon flaticon-camera19"></span> Present your app with AppSperia screenshots</a> </li>
-                            <li><a class="tooltip"><span class="icon flaticon-arrow96"></span> Change images using side arrows or caption bullets</a> </li>
-                            <li><a class="tooltip"><span class="icon flaticon-laptop3"></span> Navigate using keyboard arrows to change screenshots</a> </li>
-                        </ul>
+                        <h2>Amazing app Demo</h2>
+
                          <!-- END SECTION SCREENSHOTS CONTENT -->
                     </div>
                    
@@ -275,8 +198,8 @@
 
                     <!-- SECTION SCREENSHOTS IMAGES -->
                     <div class="slide" id="slide1" data-anchor="slide1"> <img src="images/appscreen1.png" alt="screenshot1"> </div>
-                    <div class="slide" id="slide2" data-anchor="slide2"> <img src="images/appscreen1.png" alt="screenshot2"> </div>
-                    <div class="slide" id="slide3" data-anchor="slide3"> <img src="images/appscreen1.png" alt="screenshot3"> </div>
+                    <div class="slide" id="slide2" data-anchor="slide2"> <img src="images/appscreen2.jpg" alt="screenshot2"> </div>
+                    <div class="slide" id="slide3" data-anchor="slide3"> <img src="images/appscreen3.jpg" alt="screenshot3"> </div>
                     <!-- END SECTION SCREENSHOTS IMAGES -->
                 </div>
             </div>
@@ -342,9 +265,7 @@
             <div class="wrap">
                 <div class="box">
                     <!-- SECTION DOWNLOAD CONTENT-->
-                    <h2><strong>Download</strong> the app</h2>
-                    <p>A better way to present your app using fully featured appsperia template.
-                    <br> Now available on the App Store and Play Store!</p> 
+                    <h2><strong>Download</strong> the Mongo today</h2>
                     
                     <!-- DOWNLOAD APPSTORE-->
                     <a href="#" class="simple-button appstore-button"><span class="icon appstore"></span>App Store</a>
@@ -365,24 +286,17 @@
                     <!-- SECTION CONTACT CONTENT-->
                     <h2><strong>Get</strong> in touch</h2>
                     <ul class="features">
-                        <li><a class="tooltip" href="#"><span class="icon flaticon-telephone1"></span> Call Us now<span class="tooltip-content"><span class="tooltip-text"><span class="tooltip-inner"><span class="icon flaticon-telephone1"></span>Support: +01 800.300.381 <br>Marketing: +01 800.300.382 <br>Manager: +01 800.300.383 </span></span></span></a> </li>
-                        <li><span class="tooltip"><span class="icon flaticon-map5"></span>See our location<span class="tooltip-content"><span class="tooltip-text"><span class="tooltip-inner"><span class="icon flaticon-map5"></span>2819-2835 Florence Ave, Huntington Park, CA 90255
-                            <br><a href="https://www.google.com/maps/dir/33.9736437,-118.2192335/" target="_blank">View Map</a>
-                            </span>
-                            </span>
-                            </span>
-                        </span>
-                        </li>
+                        <li><a class="tooltip" href="#"><span class="icon flaticon-telephone1"></span> Call Us now<span class="tooltip-content"><span class="tooltip-text"><span class="tooltip-inner"><span class="icon flaticon-telephone1"></span>302-648-5308 </span></span></span></a> </li>
                     </ul>
                     <ul class="features">
                         <li><span class="tooltip"><span class="icon flaticon-cursor7"></span>Official Website <span class="tooltip-content"><span class="tooltip-text"><span class="tooltip-inner"><span class="icon flaticon-cursor7"></span>
-                            <br> <a href="http://themeforest.net" target="_blank">www.appsperia.net</a>
+                            <br> <a href="http://www.allmongo.com" target="_blank">www.allmongo.com</a>
                             </span>
                             </span>
                             </span>
                             </span>
                         </li>
-                        <li><a class="tooltip" href="#"><span class="icon flaticon-mail9"></span> Send Email<span class="tooltip-content"><span class="tooltip-text"><span class="tooltip-inner"><span class="icon flaticon-mail9"></span>contact@mail.com<br>office@mail.com<br>support@mail.com</span></span></span></a> </li>
+                        <li><a class="tooltip" href="#"><span class="icon flaticon-mail9"></span> Send Email<span class="tooltip-content"><span class="tooltip-text"><span class="tooltip-inner"><span class="icon flaticon-mail9"></span>vmoore@allmongo.com </span></span></span></a> </li>
                     </ul>
 
                     <!-- SECTION CONTACT FORM-->
@@ -407,12 +321,13 @@
     <div class="wrap">
         <div id="social-icons">
             <ul>
-                <li><a href=""><i class="flaticon-facebook6"></i></a> </li>
-                <li><a href=""><i class="flaticon-social19"></i></a> </li>
-                <li><a href=""><i class="flaticon-google16"></i></a> </li>
-                <!--<li><a href=""><i class="flaticon-social40"></i></a> </li>
+                <li><a href="https://m.facebook.com/mongosocial/?tsid=0.5597999737798667&source=typeahead"><i class="flaticon-facebook6"></i></a> </li>
+                <li><a href="https://twitter.com/mongosocial?s=09"><i class="flaticon-social19"></i></a> </li>
+                <li><a href="https://plus.google.com/Sneakersoss"><i class="flaticon-google16"></i></a> </li>
+                <!--<li><a href=""><i class="flaticon-social40"></i></a> </li>-->
                 <li><a href=""><i class="flaticon-social7"></i></a> </li>
-                <li><a href=""><i class="flaticon-logo3"></i></a> </li>-->
+                <!--<li><a href=""><i class="flaticon-logo3"></i></a> </li>-->
+                
             </ul>
         </div>
     </div>
